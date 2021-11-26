877ed65a40cb43e449bc672af83c4f57b4ccffdb
AJVelezRueda <anavelezrueda@gmail.com>
2021-11-26 18:32:07 -0300

    build a db from fasta_db_file parameter

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from db_build.db_download import create_db_from_fasta
 
 
 def main():
-    create_db_from_fasta("./data/QUERY.fasta")
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fasta_db_file', 
+        type=str,
+        help='path of the Fasta file to convert to a mmseqs parseable db')
+    
+    args = parser.parse_args()
+    
+    create_db_from_fasta(args.fasta_db_file)
+    
 if __name__ == "__main__":
     main()
